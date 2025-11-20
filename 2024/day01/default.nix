bc120140383a1c05746beea36dcf22d76fbd0c15
Erlend Huru <erlendpe@online.no>
2025-11-20 19:44:56 +0100

    2024, day01, fjerner file-embed

--- 2024/day01/default.nix
+++ 2024/day01/default.nix
@@ -1,15 +1,11 @@
-{ mkDerivation, base, containers, file-embed, lib, megaparsec, mtl
-, text
-}:
+{ mkDerivation, base, containers, lib, megaparsec, mtl, text }:
 mkDerivation {
   pname = "day01";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [
-    base containers file-embed megaparsec mtl text
-  ];
+  executableHaskellDepends = [ base containers megaparsec mtl text ];
   license = lib.licenses.bsd3;
   mainProgram = "day01";
 }
