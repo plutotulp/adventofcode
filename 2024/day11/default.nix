695c70b3d1110030d626cc6d0b78b3aba64bed81
Erlend Huru <erlendpe@online.no>
2025-12-02 21:26:17 +0100

    2024, day11, default.nix

--- 2024/day11/default.nix
+++ 2024/day11/default.nix
@@ -1,12 +1,12 @@
-{ mkDerivation, base, containers, lib, text }:
+{ mkDerivation, base, containers, lib }:
 mkDerivation {
-  pname = "day10";
+  pname = "day11";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [ base containers ];
-  executableHaskellDepends = [ base text ];
+  executableHaskellDepends = [ base ];
   license = lib.licenses.bsd3;
-  mainProgram = "day10";
+  mainProgram = "day11";
 }
